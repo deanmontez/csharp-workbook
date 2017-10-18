148764f800a73eacd830b2f3c9e43a5f4e7cad97
Deandre <deandrecrayton@gmail.com>
2017-10-17 20:25:41 -0500

    Pig Latin

---
+++ 02Week/People.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Instantiate a new object
+        var human = new Person();
+
+        human.Height = 100;
+        human.Name = "Deandre";
+        human.LastName = "Crayton";
+
+        human.Greeting();
+
+        Console.ReadLine();
+    }
+}
+
+public class Person
+{
+    //Properties
+    public decimal Height { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+
+    //Methods
+    public void Greeting()
+    {
+        Console.WriteLine("Hello my name is " + Name + " " + LastName);
+    }
+}
--- 02Week/PigLatin.cs
+++ 02Week/PigLatin.cs
@@ -7,11 +7,24 @@ public class Program
         Console.WriteLine("What's the word?");
         string word = Console.ReadLine();
         Console.WriteLine(translateWord(word));
+        Console.ReadLine();
     }
     
     public static string translateWord(string word)
     {
-        // Your code here
-        return word;
+        char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y' };
+        int vowelPosition = word.IndexOfAny(vowels);
+
+        if (vowelPosition != -1)
+        {
+            string firstCharacters = word.Substring(0, vowelPosition);
+            string endOfWord = word.Substring(vowelPosition);
+            word = endOfWord + firstCharacters + "ay";
+            return word.ToLower();
+        }
+        else
+        {
+            return word;
+        }
     }
 }
